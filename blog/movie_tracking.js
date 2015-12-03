7dde76d3dadc9b3e899776131b2f9defccd46a11
thejeshgn <i@thejeshgn.com>
2015-12-03 20:54:15 +0530

    movie tracking

---
+++ blog/movie_tracking.js
@@ -0,0 +1,65 @@
+function updateGraphs(){
+	if (jQuery('#graph_display_div_id').css('display') == 'none') {
+		jQuery('#graph_display_div_id').show();
+	}else{
+		jQuery('#graph_display_div_id').hide();	
+	}
+
+	//var dt = jQuery('#tablepress-7').DataTable();
+	//var rows = dt.$('tr');
+	var rows;
+	var year_shown = [];
+	if ( jQuery.fn.dataTable.isDataTable( '#tablepress-8' ) ) {
+		//var rows = jQuery('#tablepress-7').DataTable().$('tr', {"filter":"applied"});
+		var rows = jQuery('#tablepress-8').DataTable().$('tr');
+	}else{
+		console.log("DataTable is not initialized yet");
+		return;
+	}
+	
+	var json_chart_labels = [];
+	var json_chart_movie_count = {};
+	var json_chart_movie_count_array = [];
+	//debugger;
+	for(j=0; j<rows.length; j++){
+		//var weight_date = rows[j].cells[0].innerText;
+		var watched_date = rows[j].cells[0].innerText;
+		year_month = watched_date.slice(0, 7)
+		if(json_chart_labels.indexOf(year_month) > -1){
+			movie_count = json_chart_movie_count[year_month];
+			movie_count = movie_count+1;
+			json_chart_movie_count[year_month] = movie_count;
+		}else{
+			json_chart_labels.push(year_month);
+			json_chart_movie_count[year_month] = 1;
+		}
+	}
+	var i = 0;
+	console.log(json_chart_labels);
+	for(i=0; i <= json_chart_labels.length; i++){
+		console.log(json_chart_movie_count_array);
+		json_chart_movie_count_array.push(json_chart_movie_count[json_chart_labels[i]]);
+	}
+
+
+	new Chartist.Line('#first', {
+		  labels: json_chart_labels,
+		  series: [
+			json_chart_movie_count_array,
+		  ]
+		}, {
+			    reverseData: true,
+			    fullWidth: false,
+				chartPadding: {
+					right: 60
+				},
+				axisY: {
+					onlyInteger: true
+				},
+				axisX: {
+					position: 'end',
+					showLabel: true
+				}	
+		});
+
+}
