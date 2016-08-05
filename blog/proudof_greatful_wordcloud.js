67e36b2c03195aac792fa4a736d7f16b71d07207
Thejesh GN <i@thejeshgn.com>
2016-08-06 00:32:48 +0530

    Create proudof_greatful_wordcloud.js
    added proudof_greatful_wordcloud.js

---
+++ blog/proudof_greatful_wordcloud.js
@@ -0,0 +1,40 @@
+function load_grateful(data){
+	drawCloud("grateful_canvas", data);
+}
+function load_proudof(data){
+	drawCloud("proudof_canvas", data);
+}
+
+function drawCloud(canvas_id, data){
+
+	var settings = {
+	  gridSize: Math.round(16 * jQuery("#"+canvas_id).width() / 1024),
+	  weightFactor: function (size) {
+	    return Math.pow(size, 5) * jQuery("#"+canvas_id).width() / 1024;
+	  },
+	  fontFamily: 'Times, serif',
+	  color: function (word, weight) {
+	    return (weight === 12) ? '#f02222' : '#c09292';
+	  },
+	  rotateRatio: 0.5,
+	  backgroundColor: '#ffffff'
+	};
+
+
+	var newData = [];
+	var rows = data.rows;
+	for (var i = 0; i < rows.length; i++) {
+		var d = [];
+		newData.push([rows[i]['key'],rows[i]['value'] ]);
+	}
+	console.log(newData);
+	settings['list']=newData;
+	s = WordCloud(document.getElementById(canvas_id), settings );
+
+}
+ jQuery.getJSON("https://thejeshgn.cloudant.com/proudof_grateful/_design/summary/_view/count_grateful?group=true&callback=?", function(result){
+         load_grateful(result);
+        });
+ jQuery.getJSON("https://thejeshgn.cloudant.com/proudof_grateful/_design/summary/_view/count_proud?group=true&callback=?", function(result){
+         load_proudof(result);
+        });
