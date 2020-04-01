1477323ec5db8bf0a98533783867369f44490a92
Thejesh GN <i@thejeshgn.com>
2020-04-01 13:41:10 +0530

    updated with extra functions

--- blog/project_covid19_india_non_virus_deaths.js
+++ blog/project_covid19_india_non_virus_deaths.js
@@ -16,54 +16,66 @@ function formatDateHour() {
     return [year, month, day, hour].join('-');
 }
 data_url = "https://data.thejeshgn.com/covid19/_design/india/_view/non_virus_deaths?descending=false&nounce=3"+formatDateHour();
-oTable =  jQuery('#non_virus_death_table').dataTable( {
-            "ajax": {
-                      "url":data_url,
-                      "dataSrc": "rows",
-              }, //END AJAX
-              "columns":[    //columns to display
-                  {
-                    "data":"value.incident_date",
-                    "visible": true
+
+
+function updateTable(){
+  oTable =  jQuery('#non_virus_death_table').dataTable( {
+              "ajax": {
+                        "url":data_url,
+                        "dataSrc": "rows",
+                }, //END AJAX
+                "columns":[    //columns to display
+                    {
+                      "data":"value.incident_date",
+                      "visible": true
+                    },
+                    {"data":"value.deaths"},
+                    {"data":"value.reason",
+                          "render": function(data, type, row, meta){
+                                var return_data = ""
+                                if(type === "display" || type === "filter"){
+                                    for(var i =0; i < data.length; i++){
+                                      return_data = return_data + data[i] +"<br/>";
+                                    }
+                                    
+                                }
+                                return return_data;
+                             }
+
                   },
-                  {"data":"value.deaths"},
-                  {"data":"value.reason",
-                        "render": function(data, type, row, meta){
-                              var return_data = ""
-                              if(type === "display" || type === "filter"){
-                                  for(var i =0; i < data.length; i++){
-                                    return_data = return_data + data[i] +"<br/>";
-                                  }
-                                  
-                              }
-                              return return_data;
-                           }
+                  {"data":"null",
+                          "render": function(data, type, row, meta){
+                                var return_data = ""
+                                if(type === "display" || type === "filter"){
+                                   return_data = row['value']['location'] +", "+ row['value']['district'];                                  
+                                }
+                                return return_data;
+                             }
 
-                },
-                {"data":"null",
-                        "render": function(data, type, row, meta){
-                              var return_data = ""
-                              if(type === "display" || type === "filter"){
-                                 return_data = row['value']['location'] +", "+ row['value']['district'];                                  
-                              }
-                              return return_data;
-                           }
+                  },                
+                  {"data":"value.state"},
+                  {"data":"value.source", 
+                           "render": function(data, type, row, meta){
+                                if(type === 'display'){
+                                    data = '<a  target="_blank" href="' + row['value']['source_link'] + '">' + data + '</a>';
+                                }
+                                return data;
+                             }
 
-                },                
-                {"data":"value.state"},
-                {"data":"value.source", 
-                         "render": function(data, type, row, meta){
-                              if(type === 'display'){
-                                  data = '<a  target="_blank" href="' + row['value']['source_link'] + '">' + data + '</a>';
-                              }
-                              return data;
-                           }
+                  },
+                  ],
+                  "columnDefs": [ {
+                  targets: [ 0 ]
+                  } ]                
+          } );// End: DataTable
 
-                },
-                ],
-                "columnDefs": [ {
-                targets: [ 0 ]
-                } ]                
-        } );// End: DataTable
+}
  
  
+function startEverything(){
+  updateTable();
+}
+
+
+/* Call it */
+startEverything();
