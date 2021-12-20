86ea76b9dfd4ce4b4a60ef730ec9a33f709db2f2
Thejesh GN <i@thejeshgn.com>
2021-12-20 19:37:29 +0530

    added refresh button

--- blog/blogring.js
+++ blog/blogring.js
@@ -1,16 +1,21 @@
-let api_endpoint = "https://data.thejeshgn.com/blogring/";
-let max_blogs = 12;
-var random_blog = Math.floor( (Math.random() * max_blogs) + 1) ;
+var api_endpoint = "https://data.thejeshgn.com/blogring/";
+var max_blogs = 14;
 
 function embed_blogring(data){
     content = "I recommend <a target='_blank' href='" + data["url"] + "'>" + data["title"] + "</a>. ";
-    content = content + data["description"];
+    content = content + data["description"]; 
+    content = content + "&nbsp;<span onclick ='refresh_blogring()' title='Get New Blog' style='cursor: pointer;'>♻️</span>"    
     document.getElementById("blog_ring").innerHTML = content;
 }
 
-fetch(api_endpoint+random_blog)
-  .then(response => response.json())
-  .then(
-        data => embed_blogring(data)
-    );
+function refresh_blogring(){
+  var random_blog = Math.floor( (Math.random() * max_blogs) + 1) ;
+  fetch(api_endpoint+random_blog)
+    .then(response => response.json())
+    .then(
+          data => embed_blogring(data)
+      );
+}
+
+refresh_blogring();
 
